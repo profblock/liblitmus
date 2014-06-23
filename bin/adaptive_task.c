24085929da9ab81627cf1e51de2d2c9efeab6c7b
Aaron Block <ablock@austincollege.edu>
2014-06-23 16:23:57 -0500

    Got one task to work with service levels. Need to clean up debugging code

--- bin/adaptive_task.c
+++ bin/adaptive_task.c
@@ -46,11 +46,12 @@
  * Returns 1 -> task should exit.
  *         0 -> task should continue.
  */
-int job(void);
+int job(int count);
 
 
 int test(){
-	//int do_exit;
+	int do_exit=0;
+	int i;
 	struct rt_task param;
 	struct rt_service_level oneLevel;
 	if(oneLevel.relative_work==0){
@@ -73,7 +74,26 @@ int test(){
 	
 	printf("Service Levels Beginning\n");
 	param.service_levels = (struct rt_service_level*)malloc(sizeof(struct rt_service_level)*4);
-	
+	param.service_levels[0].relative_work = 1;
+	param.service_levels[0].quality_of_service = 2.2;
+	param.service_levels[0].service_level_number = 0;
+	param.service_levels[0].service_level_period = ms2ns(PERIOD);
+
+	param.service_levels[1].relative_work = 2;
+	param.service_levels[1].quality_of_service = 3;
+	param.service_levels[1].service_level_number = 1;
+	param.service_levels[1].service_level_period = ms2ns(PERIOD);
+
+	param.service_levels[2].relative_work = 2.1;
+	param.service_levels[2].quality_of_service = 4;
+	param.service_levels[2].service_level_number = 2;
+	param.service_levels[2].service_level_period = ms2ns(PERIOD);
+
+	param.service_levels[3].relative_work = 3;
+	param.service_levels[3].quality_of_service = 5;
+	param.service_levels[3].service_level_number = 3;
+	param.service_levels[3].service_level_period = ms2ns(PERIOD);
+
 	
 	printf("Service Levels Ending\n");
 
@@ -97,10 +117,40 @@ int test(){
 	 * set_rt_task_param().
 	 */
 
+	CALL( set_rt_task_param(gettid(), &param) );
+	printf("Done setting rt_task paramaters");
 
+
+	/*****
+	 * 4) Transition to real-time mode.
+	 */
+	CALL( task_mode(LITMUS_RT_TASK) );
+	printf("Done transitioning\n ");
+
+	/* The task is now executing as a real-time task if the call didn't fail. 
+	 */
+
+	i=0;
+	printf("%d, %d\n", i, do_exit);
+	/*****
+	 * 5) Invoke real-time jobs.
+	 */ 
+	do {
+		/* Wait until the next job is released. */
+		printf("starting %d\n",i);
+		i++;
+		sleep_next_period();
+		/* Invoke job. */
+		do_exit = job(i);		
+
+	} while (!do_exit);
+
+	/*****
+	 * 6) Transition to background mode.
+	 */
 
 	CALL( task_mode(BACKGROUND_TASK) );
-	printf("done");
+	printf("done\n");
 	return 0;
 }
 /* typically, main() does a couple of things: 
@@ -148,6 +198,7 @@ int main(int argc, char** argv)
 	//param.service_levels = (struct rt_service_level*)malloc(sizeof(struct rt_service_level)*4);
 	testing = (struct rt_service_level*)malloc(sizeof(struct rt_service_level)*4);
 	//param.service_levels = &oneLevel;
+	
 
 	printf("Let's see");
 	if(testing){
@@ -188,10 +239,18 @@ int main(int argc, char** argv)
 }
 
 
-int job(void) 
+int job(int count) 
 {
 	/* Do real-time calculation. */
 	printf("Hello\n");
 	/* Don't exit. */
 	return 0;
+	/*
+	if (count>=100){
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}*/
 }
